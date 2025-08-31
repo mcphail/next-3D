34e01e609138814bd542badb1164195aaa122ba0
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2025-08-31 23:09:51 +0100

    Added fastSin and fastCos routines

--- maths.asm
+++ maths.asm
@@ -2,6 +2,8 @@
 
     			INCLUDE "globals.inc"
 
+			EXTERN	sin_table		; In ram.inc
+
 ; These are declared here
 ; https://github.com/z88dk/z88dk/tree/master/libsrc/_DEVELOPMENT/math/integer/z80n
 
@@ -64,6 +66,106 @@ negDEHL:		LD A,L
 			RET NZ
 			INC DE
 			RET
+
+; 8-bit unsigned quick multiply, with divide by 256
+; Returns A=(B*C)/256
+;
+MUL8_DIV256:		EX	DE,HL
+			LD 	D,B
+			LD	E,C
+			MUL	D,E
+			LD	A,D
+			EX	DE,HL
+			RET
+
+; extern int16_t fastSin(uint8_t a, int8_t m) __z88dk_callee;
+; extern int16_t fastCos(uint8_t a, int8_t m) __z88dk_callee;
+;
+PUBLIC _fastSin
+PUBLIC _fastCos
+
+_fastSin:		POP	BC
+			POP	DE			; D: Angle, E: Multiplier
+			LD	A,D 			; A: Angle
+			CALL	sin
+			LD	L,A
+			ADD	A,A			; Sign extend A to 16-bit value
+			SBC	A,A
+			LD	H,A
+			PUSH	BC
+			RET
+
+_fastCos:		POP	BC
+			POP	DE			; E: Angle, D: Multiplier
+			LD	A,D 
+			CALL	cos
+			LD	L,A
+			ADD	A,A			; Sign extend A to 16-bit value
+			SBC	A,A
+			LD	H,A
+			PUSH	BC
+			RET
+
+; A=COS(A)*E/256
+; A=SIN(A)*E/256
+; 
+cos:			ADD	A,64			; Cosine is a quarter revolution copy of the sin wave
+sin:			LD	H,sin_table >> 8	; The sin table is a 128 byte table on a page boundary
+			LD	L,A			; Index into the table
+			RES	7,L			; It's only a 128 byte table, so clear the top bit
+			LD 	D,(HL)			; Fetch the value from the sin table
+			RLCA				; Get the sign of the angle
+			LD	A,E			;  A: The multiplicand
+			JR	C,sin_neg_angle		; Skip to the case where the sin angle is negative
+;
+sin_pos_angle:		AND	A			; The multiplicand is also positive
+			JP	P,sin_mul_pos		; So return a positive result
+			NEG				; Otherwise negate the multiplicand
+			JR	sin_mul_neg		; And return a negative result
+;	
+sin_neg_angle:		AND	A			; The multiplicand is positive
+			JP	P,sin_mul_neg 		; So return a negative result
+			NEG 
+			JR	sin_mul_pos		; Otherwise return a positive result
+;
+sin_mul_pos:		LD	E,A			; A = +(D*A/256)
+			MUL	D,E
+			LD	A,D 
+			RET 
+;
+sin_mul_neg:		LD	E,A			; A = -(D*A/256)
+			MUL	D,E 
+			LD	A,D 
+			NEG
+			RET
+
+; Trig rotation
+; C = C*COS(A)-B*SIN(A)
+; B = C*SIN(A)+B*COS(A)
+;
+angle:			DB	0
+;
+rotate:			LD	(angle),A		; Store the angle for calculations
+			LD	E,B
+			CALL	sin			; A: B*SIN(A)
+			LD	(@M1+1),A 
+			LD	A,(angle)			
+			LD	E,C
+			CALL	cos			; A: C*COS(A)
+@M1:			SUB	A,0			; A: C*COS(A)-B*SIN(A)
+			LD	(@M3+1),A
+;
+			LD	A,(angle)
+			LD	E,B
+			CALL	cos			; A: B*COS(A)
+			LD	(@M1+2),A 
+			LD	A,(angle)			
+			LD	E,C
+			CALL	sin			; A: C*SIN(A)
+@M2:			ADD	A,0			; A: C*SIN(A)+B*COS(A)
+			LD	B,A 
+@M3:			LD	C,0
+			RET 
 
 ; extern int16_t fastMulDiv(int16_t a, int16_t b, int16_t c) __z88dk_callee
 ; Calculates a * b / c, with the internal calculation done in 32-bits
--- maths.h
+++ maths.h
@@ -1,6 +1,41 @@
-// mahts.h
+// maths.h
 #ifndef __MATHS_H__
 #define __MATHS_H__
+
+// Struct containing a 3D coordinate
+//
+typedef struct SPoint8_3D {           
+    int8_t x;
+    int8_t y;
+	int8_t z;
+} Point8_3D;
+
+typedef struct SPoint16_3D {           
+    int16_t x;
+    int16_t y;
+	int16_t z;
+} Point16_3D;
+
+// Struct containing a 3D angle
+// Assumes 256 degrees in a full rotation to make it suitable for 8-bit maths
+//
+typedef struct SAngle_3D {
+	uint8_t x;
+	uint8_t y;
+	uint8_t z;
+} Angle_3D;
+
+// Struct containing the vertice information (joining the points)
+//
+typedef struct SVertice_3D {
+	uint8_t p1;
+	uint8_t p2;
+	uint8_t p3;
+	uint8_t colour;
+} Vertice_3D;
+
+extern int16_t fastSin(uint8_t a, int8_t m) __z88dk_callee;
+extern int16_t fastCos(uint8_t a, int8_t m) __z88dk_callee;
 
 extern int16_t fastMulDiv(int16_t a, int16_t b, int16_t c) __z88dk_callee;
 
