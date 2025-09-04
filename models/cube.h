33a01c4eb50ff8d1c97192baf0c9341adb05857b
Dean Belfield <36993476+breakintoprogram@users.noreply.github.com>
2025-09-04 10:29:23 +0100

    Added colour to the faces

--- models/cube.h
+++ models/cube.h
@@ -1,4 +1,4 @@
-// Generated automatically by convert_model.py on 09/04/2025, 09:53:34
+// Generated automatically by convert_model.py on 09/04/2025, 10:26:24
 //
 // Model: cube.obj
 // Scale: 100
@@ -20,16 +20,16 @@ Point8_3D cube_p[] = {
     { -70, -70, -70 },
 };
 Vertice_3D cube_v[] = {
-    { 1, 2, 6, 0xFF },
-    { 1, 0, 3, 0xFF },
-    { 3, 0, 4, 0xFF },
-    { 5, 6, 7, 0xFF },
-    { 1, 5, 4, 0xFF },
-    { 6, 2, 3, 0xFF },
-    { 1, 6, 5, 0xFF },
-    { 1, 3, 2, 0xFF },
-    { 3, 4, 7, 0xFF },
-    { 5, 7, 4, 0xFF },
-    { 1, 4, 0, 0xFF },
-    { 6, 3, 7, 0xFF },
+    { 1, 5, 4, 0xE0 },
+    { 1, 4, 0, 0xE0 },
+    { 1, 2, 6, 0x5C },
+    { 1, 6, 5, 0x5C },
+    { 3, 0, 4, 0x07 },
+    { 3, 4, 7, 0x07 },
+    { 1, 0, 3, 0xFC },
+    { 1, 3, 2, 0xFC },
+    { 6, 2, 3, 0xA3 },
+    { 6, 3, 7, 0xA3 },
+    { 5, 6, 7, 0x1F },
+    { 5, 7, 4, 0x1F },
 };
